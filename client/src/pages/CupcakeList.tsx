5cf74eb1380e70f92ce6db09fedc78d27b5328d1
YoussefSlim <slimyoussef9511@gmail.com>
2026-02-04 14:55:20 +0100

    step II || step 2 => display cupcake list

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -2,35 +2,35 @@ import { useLoaderData } from "react-router-dom";
 import Cupcake from "../components/Cupcake";
 
 /* ************************************************************************* */
-const sampleCupcakes: CupcakeArray = [
-  {
-    id: 10,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "blue",
-    color2: "white",
-    color3: "red",
-    name: "France",
-  },
-  {
-    id: 11,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "yellow",
-    color2: "red",
-    color3: "black",
-    name: "Germany",
-  },
-  {
-    id: 27,
-    accessory_id: "5",
-    accessory: "christmas-candy",
-    color1: "yellow",
-    color2: "blue",
-    color3: "blue",
-    name: "Sweden",
-  },
-];
+// const sampleCupcakes: CupcakeArray = [
+//   {
+//     id: 10,
+//     accessory_id: "4",
+//     accessory: "wcs",
+//     color1: "blue",
+//     color2: "white",
+//     color3: "red",
+//     name: "France",
+//   },
+//   {
+//     id: 11,
+//     accessory_id: "4",
+//     accessory: "wcs",
+//     color1: "yellow",
+//     color2: "red",
+//     color3: "black",
+//     name: "Germany",
+//   },
+//   {
+//     id: 27,
+//     accessory_id: "5",
+//     accessory: "christmas-candy",
+//     color1: "yellow",
+//     color2: "blue",
+//     color3: "blue",
+//     name: "Sweden",
+//   },
+// ];
 
 /* you can use sampleCupcakes if you're stucked on step 1 */
 /* if you're fine with step 1, just ignore this ;) */
@@ -38,8 +38,7 @@ const sampleCupcakes: CupcakeArray = [
 
 function CupcakeList() {
   // Step 1: get all cupcakes
-  const getAllCupCake = useLoaderData();
-  console.log("getAllCupCake :>> ", getAllCupCake);
+  const getAllCupcakes: CupcakeArray = useLoaderData();
   // Step 3: get all accessories
 
   // Step 5: create filter state
@@ -59,10 +58,13 @@ function CupcakeList() {
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
+        {getAllCupcakes.map((cupcake) => (
+          <li className="cupcake-item">
+            <Cupcake data={cupcake} />
+          </li>
+        ))}
         {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake data={sampleCupcakes[0]} />
-        </li>
+
         {/* end of block */}
       </ul>
     </>
