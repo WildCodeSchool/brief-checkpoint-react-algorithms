17ad3f995db1f3bd0cbe8c4917647155cb2dfc62
YoussefSlim <slimyoussef9511@gmail.com>
2026-02-05 14:24:53 +0100

    step I || step 4 => algo 4

--- algo/3/getPoints.ts
+++ algo/3/getPoints.ts
@@ -12,7 +12,37 @@ Pour exemple, si ta fonction recevait le tableau ci-dessous en paramètre, tu de
 
 function getPoints(results: string[]): number {
   // Ton code ici !
-  return 0;
+  //------ Solution avec map -----//
+  //let count = 0;
+  // results.map((item) => {
+  // const split = item.split(":");
+  // input : ["3:0"].split(":")
+  // output : ["3", "0"]
+  // comparer l'elemet à l'index 0 => "3"
+  // comparer l'elemet à l'index 1 => "0"
+  // si 3 > 0 => 3pts
+  // solution avec if else
+  // if (split[0] > split[1]) {
+  //   count = count + 3;
+  // } else if (split[0] === split[1]) {
+  //   count++;
+  // }
+  // solution avec une ternire
+  // split[0] > split[1]
+  //   ? (count = count + 3)
+  //   : split[0] === split[1]
+  //     ? count++
+  //     : count + 0;
+  // solution avec destrcuturation
+  //   const [home, away] = item.split(":");
+  //   home > away ? (count = count + 3) : home === away ? count++ : count + 0;
+  // });
+  // ----Solution avec reduce--- //
+  return results.reduce((score, currentValue) => {
+    const [home, away] = currentValue.split(":");
+    home > away ? (score = score + 3) : home === away ? score++ : score + 0;
+    return score;
+  }, 0);
 }
 
 export default getPoints;
--- algo/4/sumArrays.ts
+++ algo/4/sumArrays.ts
@@ -11,7 +11,55 @@ sumArr( ["2", "5", "3"], ["2", "4", "9", "5", "5"] ) should return ["4", "9", "1
 
 function sumArr(arrayA: string[], arrayB: string[]): string[] {
   // Ton code ici !
-  return [];
+  // const array1 = ["1", "2", "3"], const array2= ["2", "4", "1"] le retour de la fonction sumArray => parcourir
+  // les tableau et faire la somme de chaque element de mes tableaux
+  // array1[1] + array2[1]. etc..
+  // initialiser un array vide
+  // const result: string[] = [];
+  // je verifie la longuer de chaque array
+  // si le length est identique
+  // if (arrayA.length === arrayB.length) {
+  //   for (let index = 0; index < arrayA.length; index++) {
+  //     const sumArray = parseInt(arrayA[index]) + parseInt(arrayB[index]);
+  //     result.push(sumArray.toString());
+  //   }
+  // }
+  //si le length sont differents
+  // arrayA = [1, 2, 3] => length = 2
+  // arrayB = [1,2,3,4] => length = 3
+  // const array = arrayA + arrayB
+  //  3 -2 = 1=>1 > 0 => arraB > arrayA
+  // si arrayA est plus long que arrayB
+  // if (arrayA.length > arrayB.length) {
+  //   const diff = arrayA.length - arrayB.length;
+  //   for (let index = 0; index < diff; index++) {
+  //     arrayB.push("0");
+  //   }
+  //   for (let index = 0; index < arrayA.length; index++) {
+  //     const sumArray = parseInt(arrayA[index]) + parseInt(arrayB[index]);
+  //     result.push(sumArray.toString());
+  //   }
+  // }
+  // // si arrayB est plus long que arrayA
+  // if (arrayA.length < arrayB.length) {
+  //   const diff = arrayB.length - arrayA.length;
+  //   for (let index = 0; index < diff; index++) {
+  //     arrayA.push("0");
+  //   }
+  //   for (let index = 0; index < arrayA.length; index++) {
+  //     const sumArray = parseInt(arrayA[index]) + parseInt(arrayB[index]);
+  //     result.push(sumArray.toString());
+  //   }
+  // }
+
+  const maxLength = Math.max(arrayA.length, arrayB.length);
+  return Array.from({ length: maxLength }, (_, i) => {
+    const valueArrayA = arrayA[i] ? parseInt(arrayA[i]) : 0;
+    const valueArrayB = arrayB[i] ? parseInt(arrayB[i]) : 0;
+    const sum = valueArrayA + valueArrayB;
+    return sum.toString();
+  });
+  // return result;
 }
 
 export default sumArr;
